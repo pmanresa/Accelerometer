6bba43c8b3f2eacb88d2ba25fd2f500f104ecfd2
Roedekro <mjacdk@gmail.com>
2015-12-03 11:35:09 +0100

    Windows and standard deviation.

--- app/src/main/java/contextawareness/accelerometer/motiondector/MainActivity.java
+++ app/src/main/java/contextawareness/accelerometer/motiondector/MainActivity.java
@@ -45,13 +45,9 @@ public class MainActivity extends Activity implements SensorEventListener {
         //Initializing arff variables
         atts = new FastVector();
 
-        atts.addElement(new Attribute("magnitude"));
-
-        /*
-        atts.addElement(new Attribute("x"));
-        atts.addElement(new Attribute("y"));
-        atts.addElement(new Attribute("z"));
-        */
+        atts.addElement(new Attribute("max"));
+        atts.addElement(new Attribute("min"));
+        atts.addElement(new Attribute("sde"));
 
         data = new Instances("MyRelation",atts,0);
 
@@ -67,6 +63,11 @@ public class MainActivity extends Activity implements SensorEventListener {
         senSensorManager.registerListener(this, senAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
+    double[] window1 = new double[128];
+    double[] window2 = new double[128];
+    int w1 = 0;
+    int w2 = 0;
+
     @Override
     public void onSensorChanged(SensorEvent event) {
         Sensor mySensor = event.sensor;
@@ -77,52 +78,80 @@ public class MainActivity extends Activity implements SensorEventListener {
             float z = event.values[2]; //Z axis
 
             // Euclidean force vector - gravity.
-            double vector = Math.sqrt(Math.pow(x,2) + Math.pow(y,2) + Math.pow(z,2)) - 9.82;
+            //double vector = Math.sqrt(Math.pow(x,2) + Math.pow(y,2) + Math.pow(z,2)) - 9.82;
 
-            double[] values = new double[data.numAttributes()];
-            values[0] = vector;
-            data.add(new Instance(1.0, values));
-        }
+            // Euclidian force vector - gravity measured on the phone (calibration).
+            double vector = Math.sqrt(Math.pow(x,2) + Math.pow(y,2) + Math.pow(z,2)) - 9.62;
 
 
-
-        /*
-        // To get a real accelerometer measurement, contribution of the force of gravity must be removed from the accelerometer data
-        float[] gravity = new float[3];
-        final float alpha = (float)0.8;
-
-        double[] values = new double[data.numAttributes()];
-
-        if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-            // Isolate the force of gravity with the low-pass filter.
-            gravity[0] = alpha * gravity[0] + (1 - alpha) * event.values[0];
-            gravity[1] = alpha * gravity[1] + (1 - alpha) * event.values[1];
-            gravity[2] = alpha * gravity[2] + (1 - alpha) * event.values[2];
-
-            // Remove the gravity contribution with the high-pass filter.
-            float x = event.values[0] - gravity[0]; //X axis
-            float y = event.values[1] - gravity[1]; //Y axis
-            float z = event.values[2] - gravity[2]; //Z axis
-
-            long curTime = System.currentTimeMillis();
-
-            if ((curTime - lastUpdate) > 100) { //If it has been 100miliseconds since the last sensor measurement update
-                long diffTime = (curTime - lastUpdate);
-                lastUpdate = curTime;
-
-                //float speed = Math.abs(x + y + z - last_x - last_y - last_z)/ diffTime * 10000; // m/s
-
-                // Saving attributes
-                values[0] = (double)x;
-                values[1] = (double)y;
-                values[2] = (double)z;
-
-                // Loading attributes to data
-                data.add(new Instance(1.0, values));
+            if(w1 < 64 && w2 == 0) {
+                window1[w1] = vector;
+                w1++;
             }
+            else {
+                window1[w1] = vector;
+                window2[w2] = vector;
+                w1++;
+                w2++;
 
+                if(w1 >= 128) {
+                    double max = getMax(window1);
+                    double min = getMin(window1);
+                    double sde = getSampleStandardDeviation(window1);
+
+                    double[] values = new double[data.numAttributes()];
+                    values[0] = max;
+                    values[1] = min;
+                    values[2] = sde;
+                    data.add(new Instance(1.0, values));
+                    w1 = 0;
+                }
+                if(w2 >= 128) {
+                    double max = getMax(window2);
+                    double min = getMin(window2);
+                    double sde = getSampleStandardDeviation(window2);
+
+                    double[] values = new double[data.numAttributes()];
+                    values[0] = max;
+                    values[1] = min;
+                    values[2] = sde;
+                    data.add(new Instance(1.0, values));
+                    w2 = 0;
+                }
+            }
         }
-        */
+    }
+
+    public double getMax(double[] a) {
+        double max = 0;
+        for(double d : a) {
+            if(d > max) max = d;
+        }
+        return max;
+    }
+
+    public double getMin(double[] a) {
+        double min = 1000;
+        for(double d : a) {
+            if(d < min) min = d;
+        }
+        return min;
+    }
+
+    public double getSampleStandardDeviation(double[] a) {
+        double mean = 0;
+        for(double d : a) {
+            mean += d;
+        }
+        mean = mean / 128;
+        double sum = 0;
+        for(double d : a) {
+            sum += Math.pow(d-mean,2);
+        }
+        sum = sum / 127; // sum / N-1, which is the sample standard deviation,
+                         // as opposed to the population standard deviation which is sum / N.
+        double sde = Math.sqrt(sum);
+        return sde;
     }
 
     @Override
@@ -141,7 +170,7 @@ public class MainActivity extends Activity implements SensorEventListener {
                 root.mkdirs();
             }
 
-            File nFile = new File(root,"motion.arff");
+            File nFile = new File(root,"motion.txt");
             FileWriter fW = new FileWriter(nFile,true);
             BufferedWriter writer = new BufferedWriter(fW);
             writer.write(data.toString());
