676bf95d3289f8f93a98970629efcac4e8954dc9
sletgaard <sunesletgaard@gmail.com>
2015-12-10 14:44:39 +0100

    Added persisted volume level for each transport mode.

--- app/src/main/java/contextawareness/accelerometer/motiondector/MainActivity.java
+++ app/src/main/java/contextawareness/accelerometer/motiondector/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -12,6 +13,7 @@ import android.hardware.SensorManager;
 import android.media.AudioManager;
 import android.os.Bundle;
 import android.os.Environment;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
@@ -32,10 +34,16 @@ import weka.core.converters.ArffSaver;
 
 public class MainActivity extends Activity {
 
+    public static final String KEY_PREF_WALK_VOLUME = "KEY_PREF_WALK_VOLUME";
+    public static final String KEY_PREF_BIKE_VOLUME = "KEY_PREF_BIKE_VOLUME";
+    public static final String KEY_PREF_BUS_VOLUME = "KEY_PREF_BUS_VOLUME";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
 
         final AudioManager audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
         boolean headsetOn = audioManager.isWiredHeadsetOn();
@@ -65,6 +73,62 @@ public class MainActivity extends Activity {
             public void onStopTrackingTouch(SeekBar seekBar) {
             }
         });
+
+        SeekBar walkingBar = (SeekBar) findViewById(R.id.walkingVolumeSlider);
+        SeekBar bikingBar = (SeekBar) findViewById(R.id.bikingVolumeSlider);
+        SeekBar busBar = (SeekBar) findViewById(R.id.busVolumeSlider);
+        walkingBar.setMax(maxVolume);
+        bikingBar.setMax(maxVolume);
+        busBar.setMax(maxVolume);
+        walkingBar.setProgress(sharedPreferences.getInt(KEY_PREF_WALK_VOLUME, maxVolume));
+        bikingBar.setProgress(sharedPreferences.getInt(KEY_PREF_BIKE_VOLUME, maxVolume));
+        busBar.setProgress(sharedPreferences.getInt(KEY_PREF_BUS_VOLUME, maxVolume));
+
+        walkingBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                SharedPreferences.Editor prefEditor = sharedPreferences.edit();
+                prefEditor.putInt(KEY_PREF_WALK_VOLUME, progress);
+                prefEditor.apply();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+        bikingBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                SharedPreferences.Editor prefEditor = sharedPreferences.edit();
+                prefEditor.putInt(KEY_PREF_BIKE_VOLUME, progress);
+                prefEditor.apply();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) { }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) { }
+        });
+        busBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                SharedPreferences.Editor prefEditor = sharedPreferences.edit();
+                prefEditor.putInt(KEY_PREF_BUS_VOLUME, progress);
+                prefEditor.apply();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) { }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) { }
+        });
+
 
         Button tmdButton = (Button) findViewById(R.id.tmdButton);
         tmdButton.setOnClickListener(new View.OnClickListener() {
